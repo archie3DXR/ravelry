4bbf1adfd253a8dae3af18bb296b13175eef9690
Archana Majumdar <archie.majumdar@gmail.com>
2023-06-02 14:55:33 +1000

    add args parser

--- 01.py
+++ 01.py
@@ -4,27 +4,36 @@
 ##create function tofetchpage with authentication
 
 import sys
+import argparse
 import requests
 import json
 
 
-def fetch_webpage():
-    url = "https://api.ravelry.com/projects/search.json?query=puppycat"
+def fetch_rav(tool, query):
+    url = f"https://api.ravelry.com/projects/{tool}.json?query={query}"
     auth = (
         "read-03d7ddeb63ba350c7f9149d489013054",
         "3x4ic3upMHHOV83s140UGpOKH9inPN+2Vvr6VmWw",
     )
     response = requests.get(url, auth=auth)
-    ravelry_things = response.json()
-    return ravelry_things
+    ravelrydata = response.json()
+    data = json.dumps(ravelrydata, indent=4)
+    print(data)
 
 
-##run fetch_webpage and print as json output
+###run fetch_webpage and print as json output
+## added args parser to handle arguments from cmd line
+## tool is search and query is search term for now
 
 if __name__ == "__main__":
-    webpage = fetch_webpage()
-    # print(webpage)
-    print(json.dumps(webpage, indent=4))
+    parser = argparse.ArgumentParser(description="Greet the specified person")
+    parser.add_argument("-tool", type=str, help="The person to greet", required=False)
+    parser.add_argument(
+        "-query", type=str, help="The age of the person", required=False
+    )
 
+    args = parser.parse_args()
 
-## 01.json is a saved json output from | jq > 01.json
+    fetch_rav(args.tool, args.query)
+
+### 01.json is a saved json output from | jq > 01.json
