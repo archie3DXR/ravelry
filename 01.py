0ed9fe8860b8d1d2e5d41bddcfd74db06e0ae161
Archana Majumdar <archie.majumdar@gmail.com>
2023-06-03 18:13:40 +1000

    added click and prettytable

--- 01.py
+++ 01.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python3
-
+import click
 from dotenv import load_dotenv
-import argparse
 import requests
 import os
 import json
 import sqlite3
+from prettytable import PrettyTable
 
 load_dotenv()
-
-# Setting up the database
 
 
 def setup_database():
     conn = sqlite3.connect('yarn_db.sqlite')
     cursor = conn.cursor()
 
-    # Create table
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS yarns (
             id INTEGER PRIMARY KEY,
             name TEXT,
             brand TEXT,
             weight_grams FLOAT,
-            weight_lbs FLOAT,
             length_meters FLOAT,
             length_yards FLOAT,
             weight_per_unit_length FLOAT
@@ -31,8 +27,6 @@ def setup_database():
     """)
     conn.commit()
     conn.close()
-
-# Fetch yarn with search query
 
 
 def fetch_rav(query):
@@ -43,17 +37,15 @@ def fetch_rav(query):
 
     try:
         response = requests.get(url, auth=auth)
-        response.raise_for_status()  # Check for any request errors
+        response.raise_for_status()
         data = response.json()
-        return data["yarns"][:10]  # Return only the first 10 results
+        return data["yarns"][:10]
     except requests.exceptions.RequestException as e:
         print(f"Request error: {e}")
         return None
     except json.JSONDecodeError as e:
         print(f"JSON decoding error: {e}")
         return None
-
-# Fetch yarn by ID
 
 
 def fetch_yarn_by_id(id):
@@ -74,8 +66,6 @@ def fetch_yarn_by_id(id):
         print(f"JSON decoding error: {e}")
         return None
 
-# Process yarn data
-
 
 def process_yarn_data(yarn_data):
     yarn_attributes = []
@@ -88,57 +78,130 @@ def process_yarn_data(yarn_data):
     yardage = yarn["yardage"]
     company_name = yarn["yarn_company"]["name"]
 
-    # Append the extracted attributes as a tuple
-    yarn_attributes.append((yarn_id, name, company_name, weight,
-                           weight * 0.00220462, yardage * 0.9144, yardage, weight / (yardage * 0.9144)))
+    yarn_attributes.append((yarn_id,
+                            name,
+                            company_name,
+                            round(weight, 2),
+                            round(yardage * 0.9144, 2),
+                            round(yardage, 2),
+                            round(weight / (yardage * 0.9144), 2)))
 
     return yarn_attributes
 
 
-# Parse arguments and fetch data
-if __name__ == "__main__":
-    setup_database()
+def view_database():
+    conn = sqlite3.connect('yarn_db.sqlite')
+    cursor = conn.cursor()
 
-    parser = argparse.ArgumentParser(description="Fetch data from Ravelry API")
-    parser.add_argument("-query", type=str,
-                        help="The query to search", required=False)
-    parser.add_argument("-id", type=int, help="The yarn id", required=False)
+    cursor.execute("SELECT * FROM yarns")
 
-    args = parser.parse_args()
+    rows = cursor.fetchall()
 
-    if args.query:
+    table = PrettyTable()
+    table.field_names = ["id", "name", "brand", "weight_grams",
+                         "length_meters", "length_yards", "weight_per_unit_length"]
 
-        data = fetch_rav(args.query)
-        if data is not None:
-            for yarn in data:  # Loop through each yarn in the data
-                print(
-                    f"Yarn ID: {yarn['id']} | {yarn['yarn_company_name']} - {yarn['name']}")
+    for row in rows:
+        table.add_row(row)
 
-        else:
-            print("No yarns found.")
+    print(table)
 
-    if args.id:
-        yarn_data = fetch_yarn_by_id(args.id)
+    conn.close()
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def search():
+    query = click.prompt("Enter your search query")
+    data = fetch_rav(query)
+    if data is not None:
+        for i, yarn in enumerate(data):
+            print(
+                f"{i+1}. Yarn ID: {yarn['id']} | {yarn['yarn_company_name']} - {yarn['name']}")
+
+        selection = click.prompt("\nSelect the yarn number", type=int)
+        yarn_id = data[selection - 1]['id']
+
+        yarn_data = fetch_yarn_by_id(yarn_id)
         if yarn_data is not None:
             yarn_attributes = process_yarn_data(yarn_data)
 
-            # Connect to the database
             conn = sqlite3.connect('yarn_db.sqlite')
             cursor = conn.cursor()
 
-            # Insert into the table
             for yarn_attr in yarn_attributes:
                 cursor.execute(
                     "SELECT COUNT(*) FROM yarns WHERE id = ?", (yarn_attr[0],)
                 )
                 if cursor.fetchone()[0] > 0:
                     cursor.execute(
-                        "UPDATE yarns SET name = ?, brand = ?, weight_grams = ?, weight_lbs = ?, length_meters = ?, length_yards = ?, weight_per_unit_length = ? WHERE id = ?",
-                        (yarn_attr[1], yarn_attr[2], yarn_attr[3], yarn_attr[4], yarn_attr[5], yarn_attr[6], yarn_attr[7], yarn_attr[0]))
+                        "UPDATE yarns SET name = ?, brand = ?, weight_grams = ?, length_meters = ?, length_yards = ?, weight_per_unit_length = ? WHERE id = ?",
+                        (yarn_attr[1], yarn_attr[2], yarn_attr[3], yarn_attr[4], yarn_attr[5], yarn_attr[6], yarn_attr[0]))
                 else:
                     cursor.execute(
-                        "INSERT INTO yarns (id, name, brand, weight_grams, weight_lbs, length_meters, length_yards, weight_per_unit_length) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                        "INSERT INTO yarns (id, name, brand, weight_grams, length_meters, length_yards, weight_per_unit_length) VALUES (?, ?, ?, ?, ?, ?, ?)",
                         yarn_attr)
 
             conn.commit()
             conn.close()
+
+
+@cli.command()
+def view():
+    view_database()
+
+
+@cli.command()
+def delete():
+    view_database()  # Display the existing records
+    yarn_id = click.prompt("Enter the yarn id to delete", type=int)
+
+    conn = sqlite3.connect('yarn_db.sqlite')
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "DELETE FROM yarns WHERE id = ?", (yarn_id,)
+    )
+    conn.commit()
+
+    print(f"Deleted yarn with ID: {yarn_id}")
+    conn.close()
+
+
+if __name__ == "__main__":
+    setup_database()
+    cli()
+
+
+# ________Not Inc_________________________________
+
+
+@cli.command()
+def add():
+    yarn_id = click.prompt("Enter the yarn id", type=int)
+    yarn_data = fetch_yarn_by_id(yarn_id)
+    if yarn_data is not None:
+        yarn_attributes = process_yarn_data(yarn_data)
+
+        conn = sqlite3.connect('yarn_db.sqlite')
+        cursor = conn.cursor()
+
+        for yarn_attr in yarn_attributes:
+            cursor.execute(
+                "SELECT COUNT(*) FROM yarns WHERE id = ?", (yarn_attr[0],)
+            )
+            if cursor.fetchone()[0] > 0:
+                cursor.execute(
+                    "UPDATE yarns SET name = ?, brand = ?, weight_grams = ?, length_meters = ?, length_yards = ?, weight_per_unit_length = ? WHERE id = ?",
+                    (yarn_attr[1], yarn_attr[2], yarn_attr[3], yarn_attr[4], yarn_attr[5], yarn_attr[6], yarn_attr[0]))
+            else:
+                cursor.execute(
+                    "INSERT INTO yarns (id, name, brand, weight_grams, length_meters, length_yards, weight_per_unit_length) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                    yarn_attr)
+
+        conn.commit()
+        conn.close()
